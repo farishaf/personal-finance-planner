71883413ac55fdd179fe5a6328f31ed718d83ea9
farishhaf <farishassan910@gmail.com>
2025-06-25 14:53:32 +0700

    feat: add settings page layout, update folder naming

---
+++ app/settings/page.tsx
@@ -0,0 +1,21 @@
+"use client"
+
+import PageTitle from '@/components/page-title';
+import React from 'react'
+
+const SettingsPage = () => {
+
+  return (
+    <div className="flex flex-col p-4 space-y-4">
+      <PageTitle
+        title="Settings"
+        subTitle="Manage your tags and placements"
+      />
+
+      {/* to do: add settings components here */}
+
+    </div>
+  )
+}
+
+export default SettingsPage;
